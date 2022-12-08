90b8cbff220bf76901b3f0f157875db3d777449b
mgemoraw <meng.get4@gmail.com>
2022-12-08 02:18:21 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
 int main(void)
 {
-	char string[] = "\"Programming is like building a multilinqual puzzle";
 
-	puts(string);
+	puts("\"Programming is like building a multilinqual puzzle");
 	return (0);
 }

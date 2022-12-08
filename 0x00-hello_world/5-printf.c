9e8858a91df7a3d9e1578ee03ab40804fe633e58
mgemoraw <meng.get4@gmail.com>
2022-12-08 02:25:39 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "with proper grammer, but the outcome is  apiece of art"
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}

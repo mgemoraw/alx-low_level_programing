4486b3d95c3bd5336512af767e95271891b58538
mgemoraw <meng.get4@gmail.com>
2022-12-08 11:54:53 -0800

    final update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
 * main - Entering
 *
 * Description: prints "and that piece of art is userful\" - Dora Korpar"
-* Return: 1
-* /
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);

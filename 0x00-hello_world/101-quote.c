6525f9b3fb2e3359173a044ea40e627041afc81b
mgemoraw <meng.get4@gmail.com>
2022-12-08 11:50:26 -0800

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
 * main - Entering
 *
-* Description: prints "and that piece of art is userful\" - Dora Korpar, 2 015-10-19\n"
+* Description: prints "and that piece of art is userful\" - Dora Korpar"
 */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

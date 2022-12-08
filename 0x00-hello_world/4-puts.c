c745deee477b74295a0a72fe412cd3093f2280cb
mgemoraw <meng.get4@gmail.com>
2022-12-08 02:08:32 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 int main(void)
 {
-	char string[] = "Programming is like building a multilingual puzzle.";
+	char string[] = "\"Programming is like building a multilinqual puzzle";
 
 	puts(string);
-	
 	return (0);
 }

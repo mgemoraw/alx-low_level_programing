ad7db7ea6027adf078066e2ca787b195b553dddc
mengist <meng.get4@gmail.com>
2022-12-09 23:16:19 +0300

    Numberz:

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: prints  alphabets using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i+'0');
+	}
+	putchar('\n');
+
+	return (0);
+}

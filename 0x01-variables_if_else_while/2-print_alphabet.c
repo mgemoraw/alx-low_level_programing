080230fe426e352f8d38b9295a952b0aadc6161a
mengist <meng.get4@gmail.com>
2022-12-09 22:39:17 +0300

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: prints  alphabets using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}

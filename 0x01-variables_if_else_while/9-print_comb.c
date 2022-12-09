de0229f8be7d7df41a795d0be04c2f0906dcf11e
mengist <meng.get4@gmail.com>
2022-12-09 23:52:47 +0300

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: prints  alphabets using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i;
+	char ch;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}

7952217f9a5efcecd21da889e607d4a5952b4554
mengist <meng.get4@gmail.com>
2022-12-10 00:23:15 +0300

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: prints  alphabets using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (int j = i; j <= 9; j ++)
+		{
+			if(i != j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

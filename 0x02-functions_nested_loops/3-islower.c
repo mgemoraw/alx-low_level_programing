883584b81aa1eec25cafafbdcf192f558e035922
mengist <meng.get4@gmail.com>
2022-12-13 22:38:10 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _islower - print alphabet lowercase
- *
+ * _islower - checks lowercase
+ * @c: contains value to be compared
  * Return: Always 0.
  */
 
 int _islower(int c)
 {
-	if (c >= 97 && c <=122)
-		return 1;
+	if (c >= 97 && c <= 122)
+		return (1);
 	else
-		return 0;
+		return (0);
 
 }

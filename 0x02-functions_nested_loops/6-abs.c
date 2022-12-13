f649089fef12e35391c179a538f96293f87f3a65
mengist <meng.get4@gmail.com>
2022-12-13 23:24:30 +0300

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - checks the absolute value
+ * @n: contains value to be checked
+ * Return: Always 0.
+ */
+
+int _isalpha(int n)
+{
+	int ans;
+	if (n < 0)
+		return (-1*n);
+	else
+		return (n);
+
+}

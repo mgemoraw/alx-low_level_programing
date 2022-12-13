04259d1807b7c6b19e5639eec2f78bf09a834a78
mengist <meng.get4@gmail.com>
2022-12-14 02:21:41 +0300

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,8 +5,9 @@
  * @a: input parameter a
  * @b: input parameter b
  * Description: add two numbers and returns their sum
- * Return : sum of a and b
+ * Return: sum of a and b
  */
+
 int add(int a, int b)
 {
 	return (a + b);

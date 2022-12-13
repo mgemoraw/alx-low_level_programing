ab9602865994540aea9ff043d89808cbc53a6350
mengist <meng.get4@gmail.com>
2022-12-13 22:53:57 +0300

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks alphabet characters
+ * @n: contains value to be checked
+ * Return: Always 0.
+ */
+
+int _isalpha(int n)
+{
+	if (n >= 65 && n <= 122)
+		return (1);
+	else if (n == '0')
+
+	else if (n < 0)
+		return (-1);
+
+}

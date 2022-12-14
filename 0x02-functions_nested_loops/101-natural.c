4d3331531bb374b1d914d75b0c058e454dc4b8d7
mengist <meng.get4@gmail.com>
2022-12-14 23:23:48 +0300

    Natural made the natural numbers; All esle is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
  * print_times_table - prints time table
  * @n: input parameter
  * Return: Always 0.
  */
-void print_natural(void)
+int main(void)
 {
 	int a;
 	int sum = 0;
 	for (a = 0; a < 1024; a++)
 	{
-		if (a %5 == 0 || a % 3 == 0)
+		if ((a % 5 == 0) || (a % 3 == 0))
 		{
 			sum += a;
 		}

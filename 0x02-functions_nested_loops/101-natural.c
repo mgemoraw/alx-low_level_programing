d03e31f00067b3f655e717c8f8929903be469d4d
mengist <meng.get4@gmail.com>
2022-12-14 23:26:39 +0300

    Natural made the natural numbers; All esle is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,11 +10,7 @@ int main(void)
 	int a;
 	int sum = 0;
 	for (a = 0; a < 1024; a++)
-	{
 		if ((a % 5 == 0) || (a % 3 == 0))
-		{
 			sum += a;
-		}
-	}
-	printf("%d\n", sum);
+	printf("%d", sum);
 }

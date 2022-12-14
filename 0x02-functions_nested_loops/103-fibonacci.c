cd607f3206a9785306373da6bc5018784568e1c3
mengist <meng.get4@gmail.com>
2022-12-15 00:19:12 +0300

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- prints sum of even fibonacci numbers
+ * fibonacci sum 
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long int a = 0, b = 1, c;
+	unsigned long int sum = 0;
+	for (i = 0; i < 50; i++)
+	{
+		c = a + b;
+		if (c % 2 == 0)
+		{
+			sum += c;
+		}
+
+		a = b;
+		b = c;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
+
+
+

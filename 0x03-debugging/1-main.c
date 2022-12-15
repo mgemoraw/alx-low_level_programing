6640f8ba4c5805ff07733cf08f036dedccf81ace
mengist <meng.get4@gmail.com>
2022-12-15 20:11:08 +0300

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - tests that prints if integer is negative or positive
+ * Return: 0
+*/
+
+int main(void)
+{
+
+int i;
+/*
+*while (i < 10)
+*{
+*putchar(i)
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}

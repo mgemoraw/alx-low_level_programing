b5722f680a14d51759b1dcf01c780f5837b7b6fb
mengist <meng.get4@gmail.com>
2022-12-15 09:18:22 +0300

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+ * _isdigit - function checks digit numbers
+ * @c: input character
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 48 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

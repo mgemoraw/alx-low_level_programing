00c2ed159cb004fe8033a33bca38424609ef4243
mengist <meng.get4@gmail.com>
2022-12-15 23:33:18 +0300

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - prints number 1 100
+*Description: prints 1 - 100
+*Return: returns 0
+*/
+int main(void)
+{
+    int num;
+    for (num = 1; num <= 100; num++)
+    {
+        if(num % 3 == 0)
+        {
+            printf("%s ", "Fizz");
+        }
+        else if (num % 5 == 0)
+        {
+            printf("%s ", "Buzz");
+        }
+        else
+            printf("%d ", num);
+    }
+    printf("\n");
+
+    return (0);
+}

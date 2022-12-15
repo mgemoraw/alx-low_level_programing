0fc289660f216f8f856441866fe7269ed0d16158
mengist <meng.get4@gmail.com>
2022-12-15 22:49:55 +0300

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,18 @@
 
 void print_diagonal(int n)
 {
-int num1, num2;
-if (n > 0)
-{
-for (num1 = 0; num1 < n; num1++)
-{
-for (num2 = 0; num2 < num1; num1++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-_putchar('\n');
-}
-}
-_putchar(10);
+    int num1, num2;
+    if (n > 0)
+    {
+        for (num1 = 0; num1 < n; num1++)
+        {
+            for (num2 = 0; num2 < num1; num1++)
+            {
+                _putchar(' ');
+            }
+            _putchar('\\');
+            _putchar('\n');
+        }
+    }
+    _putchar('\n');
 }

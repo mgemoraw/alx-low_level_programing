b3e27c1d23280664abbe09a46b08d0855af761b3
mengist <meng.get4@gmail.com>
2022-12-15 21:45:17 +0300

    Numbers constitute the ony universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,17 @@
 
 void more_numbers(void)
 {
-    int num, man;
-    for (num = 0; num < 10; num++)
-    {
-        for (man = 0; man < 15; man++)
-        {
-            if (man > 9)
-            {
-                _putchar(man / 10 + '0');
-            }
-            _putchar(man % 10 + '0');
-        }
-        _putchar(10);
-    }
+int num, man;
+for (num = 0; num < 10; num++)
+{
+for (man = 0; man < 15; man++)
+{
+if (man > 9)
+{
+_putchar(man / 10 + '0');
+}
+_putchar(man % 10 + '0');
+}
+_putchar(10);
+}
 }

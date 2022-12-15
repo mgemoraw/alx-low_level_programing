02a9185bc611e59b59c7a98a159fc4732a66fc2b
mengist <meng.get4@gmail.com>
2022-12-15 20:57:17 +0300

    The numbers speack for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/* more headers goes there */
+
+/**
+ * mul - function multiplies two digit numbers
+ * @a: input parameter
+ * @b: input character
+ * Return: returns multiple of a and b
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}

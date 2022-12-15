1475e1c87a85c97d08b449b89d70318442c00629
mengist <meng.get4@gmail.com>
2022-12-16 00:50:55 +0300

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,14 +10,14 @@ int i, j, k;
 
 if (size > 0)
 {
-for (i = 0; i < size; i++)
+for (i = 0; i <= size; i++)
 {
-for (j = size; j >= i; j--)
+for (j = size - i; j >= 0; j--)
 {
 _putchar(' ');
 }
 
-for (k = 0; k <= i; k++)
+for (k = 0; k < i; k++)
 {
 _putchar('#');
 }

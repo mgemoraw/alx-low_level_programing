d23cc2338b2436d3e78dd83eb5c38736943e2757
mgemoraw <meng.get4@gmail.com>
2022-12-20 01:24:39 +0300

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <string.h>
+
 /**
-* _puts2 - prints a string
+* puts2 - prints a string
 * @str: input string
 * Description: this prints prints strings
 * Return: returns nothing
 */
-void _puts2(char *str)
+void puts2(char *str)
 {
 int c = 0;
 
 while (str[c] != '\0')
 {
-if (c%2 == 0)
+if (c % 2 == 0)
 _putchar(str[c]);
 c += 1;
 }

bdd067869af08d44d189175940b579f018e8a8a4
mgemoraw <meng.get4@gmail.com>
2022-12-20 00:45:01 +0300

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* rev_string - reverses string a string
-* @s: input string
+* puts_half - prints half string of a string
+* @str: input string
 * Description: this prints prints strings
 * Return: returns nothing
 */
@@ -13,21 +13,20 @@ int len = 0, n, i;
 while (str[len] != '\0')
 {
 len++;
-}  
+}
 
 if (len % 2 == 0)
 {
-    n = (len) / 2;
+n = (len) / 2;
 }
 else
 {
-    n = (len- 1) / 2;
+n = (len - 1) / 2;
 }
-for (i = n; i < len; i++ )
+for (i = n; i < len; i++)
 {
-    _putchar(str[i]);
-} 
-
+_putchar(str[i]);
+}
 
 _putchar('\n');
 

ca221c564116b964944c339f74a06062ec58710e
mgemoraw <meng.get4@gmail.com>
2022-12-19 19:49:39 +0300

    Hello Array as and pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* reset_to_98: resets the number n to 98
+* @n: input integer
+* Return: returns nothing
+*/
+
+void reset_to_98(int *n)
+{
+
+    int *p = &n;
+    *p = 98;
+}

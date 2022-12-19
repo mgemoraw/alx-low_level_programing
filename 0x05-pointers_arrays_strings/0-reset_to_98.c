e6c9d141f2b997d7a9719a122c707ca24f8f8f3c
mgemoraw <meng.get4@gmail.com>
2022-12-19 20:00:52 +0300

    0-98 Battery St.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,13 @@
 /**
 * reset_to_98: resets the number n to 98
 * @n: input integer
+* Description: this function resets to 98
 * Return: returns nothing
 */
 
 void reset_to_98(int *n)
 {
 
-    int *p = &n;
-    *p = 98;
+int *p = n;
+*p = 98;
 }

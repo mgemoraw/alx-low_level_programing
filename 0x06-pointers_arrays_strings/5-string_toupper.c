96b02483cc4aa1f06e96e46a287abdfd56a547e4
mgemoraw <meng.get4@gmail.com>
2022-12-22 23:53:32 +0300

    string to uppoer

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* string_toupper - changes lower to upper case letter
+* @ch: input character string
+* Return: length of a string
+*/
+char *string_toupper(char *str)
+{
+int i = 0;
+
+while (str[i] >= 65 && str[i] <= 122)
+{
+    if (str[i] >= 97)
+    {
+        str[i] -= 32;
+    }
+    else
+        str[i] += 0;
+    i++;
+}
+return (str);
+}

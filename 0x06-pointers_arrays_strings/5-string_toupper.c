7736a9a16db412838e4e9695ee24d443644fa091
mgemoraw <meng.get4@gmail.com>
2022-12-23 00:01:13 +0300

    Always look up!

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+* _strlen - computes length of a string
+* @str: input string
+* Return: length of a string
+*/
+int _strlen(char *str)
+{
+int c = 0;
+while (str[c] != '\0')
+{
+c++;
+}
+return (c);
+}
+
+/**
 * string_toupper - changes lower to upper case letter
 * @str: input character string
 * Return: length of a string
@@ -8,10 +23,11 @@
 char *string_toupper(char *str)
 {
 int i = 0;
+int len = _strlen(str);
 
-while (str[i] >= 65 && str[i] <= 122)
+while (i < len)
 {
-if (str[i] >= 97)
+if (str[i] >= 'a' && str[i] <= 'z')
 {
 str[i] -= 32;
 }

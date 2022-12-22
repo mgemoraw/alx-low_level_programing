66cd8397b6ad21c0c5e329b4d74932b783824848
mgemoraw <meng.get4@gmail.com>
2022-12-23 00:53:45 +0300

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+* _strlen - computes length of a string
+* @str: input string
+* Return: length of a string
+*/
+int _strlen(char *str)
+{
+int c = 0;
+while (str[c] != '\0')
+{
+c++;
+}
+return (c);
+}
+
+/**
+* cap_string - changes lower to upper case letter
+* @str: input character string
+* Return: length of a string
+*/
+char *cap_string(char *str)
+{
+int i = 0;
+
+while (str[++i])
+	{
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+i++;
+
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == ';' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}')
+str[i] -= 32;
+}
+return (str);
+}

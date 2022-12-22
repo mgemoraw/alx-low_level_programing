7bb03cbaf85b610e7a29713c2753cea4eea50bd2
mgemoraw <meng.get4@gmail.com>
2022-12-22 22:49:04 +0300

    strcmp:

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -28,16 +28,12 @@ int _strcmp(char *s1, char *s2)
 
 int i = 0, sum = 0;
 
-while (s1[i] != '\0' && s2[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0' && *s1 != *s2)
 {
 if (s1[i] >= s2[i])
 {
 sum += s1[i] - s2[i];
 return (sum);
-}
-else if (s1[i] == s2[i])
-{
-sum += s1[i] - s2[i];
 }
 else
 {

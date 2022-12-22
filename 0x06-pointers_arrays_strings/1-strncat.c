40394c61bcebe5ab9cda7ee812592b1df32e2120
mgemoraw <meng.get4@gmail.com>
2022-12-22 21:47:10 +0300

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _strlen - computes length of a string
+* @str: input string
+* Return: length of a string
+*/
+int _strlen(char *str)
+{
+int c = 0;
+while (str[c] != '\0')
+{
+c++;
+}
+return (c);
+}
+
+/**
+* _strcat - concatenates two strings
+* @dest: destination string
+* @src: source string
+* @n: input position integer
+* Description: this function resets to 98
+* Return: returns nothing
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+int d = _strlen(dest);
+/*int s = _strlen(src); */
+int i = n;
+while (src[i] != '\0')
+{
+dest[d] = src[i];
+i++;
+d++;
+}
+dest[d] = '\0';
+return (dest);
+}

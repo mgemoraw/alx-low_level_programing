ccae568790ebdf66c65f0ac0a216eb917477b1bc
mgemoraw <meng.get4@gmail.com>
2022-12-28 02:24:43 +0300

    Fibonacci last try

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,15 @@
  *
  * Owner By mgemoraw
  */
-
 int main(void)
 {
 	int i;
 	unsigned long int a = 0, b = 1, c;
 	unsigned long int num1, num2, num3, num4, f1, f2;
 
-	for (i = 0; i < 93; i++)
+	for (i = 0; i < 92; i++)
 	{
-		
+
 		c = a + b;
 		printf("%lu, ", c);
 		a = b;
@@ -27,7 +26,7 @@ int main(void)
 	num3 = a % 10000000000;
 	num4 = b % 10000000000;
 
-	for ( i = 93; i < 98; i++)
+	for (i = 92; i < 98; i++)
 	{
 		f1 = num1 + num2;
 		f2 = num3 + num4;
@@ -36,21 +35,15 @@ int main(void)
 			f1 += 1;
 			f2 %= 10000000000; /*10,000,000,000 */
 		}
-		printf("%lu%lu", f1, f2);	
+		printf("%lu%lu", f1, f2);
 		if (i != 97)
-		{
 			printf(", ");
-		}
 		else
-		{
 			printf("\n");
-		}
-
 		num1 = num2;
 		num3 = num4;
 		num2 = f1;
-		num4 = f2;	
+		num4 = f2;
 	}
-
 	return (0);
 }

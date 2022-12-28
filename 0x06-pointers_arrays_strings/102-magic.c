8beda53730a38803a1f7378a91af9c6312d89690
mgemoraw <meng.get4@gmail.com>
2022-12-28 04:05:03 +0300

    Hello magic code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-/** main - function */
+/*
+* main - function prints 98
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 int main(void)
 {
 int n;
@@ -8,14 +16,6 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
 p = a;*(p+2) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);

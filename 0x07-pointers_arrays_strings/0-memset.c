f5ce4d5932c7c18b2518bc87c3ac5d99283d3bd6
mgemoraw <meng.get4@gmail.com>
2023-01-02 20:50:09 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include 'main.h'
+
+/**
+* _memset - fills memory
+* @s: input string
+* @b: fill back character
+* @n: number of fills required
+* Return: returns pointer string s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    int c;
+    for(c = 0; (s[c] != '\0') && (c < n); c++)
+    {
+        *(s+c) = b;
+    }
+    return s;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+This section deals with more deep diving into pointers and multidimentional arrays.
+Problems are solved by understanding given problems and built-in methods provided by the c-standard library.
+Enjoy it
+Autor of Solution is goes to mgemoraw/Mengistu

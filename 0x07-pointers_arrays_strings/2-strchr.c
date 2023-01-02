417ca3358e4befd02abf1913d6260fff01734e43
mgemoraw <meng.get4@gmail.com>
2023-01-03 00:02:39 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,20 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
 * _strchr - searchs for character c
 * @s: input string
 * @c: input character
-* Return: returns pointer s
+* Return: returns pointer string from c
 */
 
 char *_strchr(char *s, char c)
 {
 unsigned int i;
+char ch = c;
 for (i = 0;; i++)
 {
-if (*(s + i) == c)
+if (*(s + i) == ch)
 {
 return (s + i);
 }

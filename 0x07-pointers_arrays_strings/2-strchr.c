4c566bf4c5c38b776451274454d5e08f38d3751c
mgemoraw <meng.get4@gmail.com>
2023-01-02 22:12:09 +0300

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strchr - searchs for character c
+* @s: input string
+* @c: input character
+* Return: returns pointer s
+*/
+
+char *_strchr(char *s, char c)
+{
+int i, j, k = 0;
+
+for (i = 0; (s[i] != '\0'); i++)
+{
+if (*(s + i) == c)
+break;
+}
+
+for (j = i; s[j] != '\0'; j++)
+{
+*(s + k) = *(s + j);
+k++;
+
+}
+s[k] = '\0';
+return (s);
+}

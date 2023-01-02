a7fdf12417e09d41304028461d3b9aa7a6a00c8a
mgemoraw <meng.get4@gmail.com>
2023-01-02 21:02:29 +0300

    Memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include "main.h"
 
 /**
 * _memset - fills memory
@@ -10,7 +10,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-int c;
+unsigned int c;
 for (c = 0; (s[c] != '\0') && (c < n); c++)
 {
 *(s + c) = b;

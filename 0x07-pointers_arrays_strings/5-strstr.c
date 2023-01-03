c5177a8cdf73ae55a1d347eec93e8a7efc9f3e07
mgemoraw <meng.get4@gmail.com>
2023-01-03 10:08:13 +0300

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include<stddef.h>
+
+/**
+* _strstr - locates the substring
+* @haystack: input string
+* @needle: comparision string
+* Return: returns pointer to string
+* Author: mgemoraw
+*/
+char *_strstr(char *haystack, char *needle)
+{
+for (;; ++needle)
+{
+for (;; ++haystack)
+{
+if (*needle == *haystack)
+return ((char *)needle);
+if (*needle == '\0')
+return (NULL);
+}
+}
+}

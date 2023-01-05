df4acf7beb4a76d4e4271890afb93e37d969be68
mgemoraw <meng.get4@gmail.com>
2023-01-05 06:26:20 +0300

    recursion correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,13 @@
 */
 void _puts_recursion(char *s)
 {
-for (; *s != '\0'; ++s)
+int i = 0;
+if (*s == '\0')
 {
-_putchar(*s);
+return;
 }
+_putchar(*s);
+_puts_recursion(s++);
 _putchar('\n');
 
 }

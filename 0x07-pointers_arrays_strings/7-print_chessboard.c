8850b940ebac4da68fba6429f66ed9ae76d0bcc5
mgemoraw <meng.get4@gmail.com>
2023-01-05 16:42:47 +0300

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_chessboard - prints chessboard
+* @a: a pointer array
+* Return: returns pointer to string
+* Author: mgemoraw
+*/
+void print_chessboard(char (*a)[8])
+{
+    
+}

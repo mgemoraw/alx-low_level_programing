9c43747735cdf1b41ef2647037062fd5308d25e9
mgemoraw <meng.get4@gmail.com>
2023-01-06 22:59:37 +0300

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints name of function
+* @argc: argument coun
+* @argv: argument vector
+* Return: returns always zero
+* Aughor: mgemoraw
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[1]);
+return (0);
+}

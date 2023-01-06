91f195bb7b43052c77c5a6e760cb5a12c89c8bd6
mengist <meng.get4@gmail.com>
2023-01-07 01:12:04 +0300

    refineing my code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
 * main - prints name of function
@@ -7,7 +8,7 @@
 * Return: returns always zero
 * Aughor: mgemoraw
 */
-int main(int argc, char *argv[])
+int _main(int argc, char *argv[])
 {
 	int i;
 	printf("%s\n", argv[0]);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _main(int argc, char *argv[]);
 
 
 #endif

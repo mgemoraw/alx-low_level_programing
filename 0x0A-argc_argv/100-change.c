c85ccfe0c61ff692f8099b8ff5c77895632aae51
mengist <meng.get4@gmail.com>
2023-01-09 00:47:21 +0300

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,41 +12,41 @@
 
 int main(int argc, char *argv[])
 {
-    int cents, coins = 0;
-    cents = atoi(argv[1]);
+int cents, coins = 0;
+cents = atoi(argv[1]);
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    while (cents > 0)
-    {
-        coins++;
-        if((cents - 25) >= 0)
-        {
-            cents -= 25;
-            continue;
-        }
-        if((cents - 10) >= 0)
-        {
-            cents -= 10;
-            continue;
-        }
-        if((cents - 5) >= 0)
-        {
-            cents -= 5;
-            continue;
-        }
-        if((cents - 2) >= 0)
-        {
-            cents -= 2;
-            continue;
-        }
-        cents--;
-    }
-    printf("%d\n", coins);
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+while (cents > 0)
+{
+coins++;
+if((cents - 25) >= 0)
+{
+cents -= 25;
+continue;
+}
+if((cents - 10) >= 0)
+{
+cents -= 10;
+continue;
+}
+if((cents - 5) >= 0)
+{
+cents -= 5;
+continue;
+}
+if((cents - 2) >= 0)
+{
+cents -= 2;
+continue;
+}
+cents--;
+}
+printf("%d\n", coins);
 
-    return (0);
+return (0);
     
 }

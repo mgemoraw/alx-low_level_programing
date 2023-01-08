4b9866190c2d6cd2b3ca9dea1b5b7a06897cc07a
mgemoraw <meng.get4@gmail.com>
2023-01-08 16:43:35 +0300

    unmerged content

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
@@ -16,22 +15,3 @@ int main(int __attribte__((__unused___)) argc, char *argv[])
 	printf("%s\n", argv[0]);
 	return (0);
 }
-
-=======
-#include <stdio.h>
-
-/**
-* main - prints name of function
-* @argc: argument coun
-* @argv: argument vector
-* Return: returns always zero
-* Aughor: mgemoraw
-*/
-
-int main(int __attribte__((__unused___)) argc, char *argv[])
-{
-	printf("%s\n", argv[0]);
-	return (0);
-}
-
->>>>>>> 05fa4bcb6a4e6abdf7ce8b55f4ae54a560cce9ba

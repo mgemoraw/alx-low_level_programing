faba57c8119be63de27dde6f178147153bb891f6
mengist <meng.get4@gmail.com>
2023-01-08 22:38:02 +0300

    final commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
 * main - function prints number of args
@@ -9,13 +11,15 @@
 * Author: mgemoraw
 */
 
+int digit(char *s);
+
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-	if (atoi(argv[i]))
+	if (digit(argv[i]) == 1)
 	{
 		sum += atoi(argv[i]);
 	}
@@ -29,4 +33,27 @@ int main(int argc, char *argv[])
 	return (0);
 }
 
+/**
+* digit - checks for digit
+* @s: input string
+* Return: returns integer
+*/
+int digit(char *s)
+{
+	unsigned int i, re = 0;
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (isdigit(s[i]))
+		{
+			re = 1;
+		}
+		else
+		{
+			re = 0;
+			break;
+		}
+		
+	}
+	return (re);
 
+}

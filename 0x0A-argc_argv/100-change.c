847ca3a4d85639bf12e69e27e7c370ab510d51f3
mengist <meng.get4@gmail.com>
2023-01-09 01:02:23 +0300

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
+/**
+* digit - checks for digit
+* @s: input string
+* Return: returns integer
+*/
+int digit(char *s)
+{
+	unsigned int i, re = 0;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (isdigit(s[i]))
+		{
+			re = 1;
+		}
+		else
+		{
+			re = 0;
+			break;
+		}
+
+	}
+	return (re);
+}
 /**
 * main - function prints number of args
 * @argc: argument count
@@ -13,15 +38,10 @@
 int main(int argc, char *argv[])
 {
 int cents, coins = 0;
-cents = atoi(argv[1]);
 
-if (argc != 2)
+if (digit(argv[1]) == 1 && argc == 2)
 {
-printf("Error\n");
-return (1);
-}
-else
-{
+cents = atoi(argv[1]);
 while (cents > 0)
 {
 coins++;
@@ -49,6 +69,11 @@ continue;
 cents--;
 }
 printf("%d\n", coins);
+}
+else
+{
+printf("Error\n");
+return (1);
 }
 return (0);
 

af3166dd7dee6cef521b701c44ede3a584b36eeb
mengist <meng.get4@gmail.com>
2023-01-09 00:20:25 +0300

    Changing coins to money

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+* cents - function prints number of args
+* @s: input character
+* Return: returns 0
+* Author: mgemoraw
+*/
+int cents(char *s)
+{
+    unsigned int cent atoi(s);
+    int cnum = 0;
+
+}
+
+/**
+* main - function prints number of args
+* @argc: argument count
+* @argv: argument array
+* Return: returns 0
+* Author: mgemoraw
+*/
+
+int main(int argc, char *argv[])
+{
+    int i, count = 0;
+    char *s = argv[1];
+    if (atoi(argv[1]))
+    for(i = 0; *s != '\0'; i++)
+    {
+
+    }
+}

0f43fef4e100b4fe558b9ab318ec3e152d1d8cb0
mgemoraw <meng.get4@gmail.com>
2023-01-11 05:22:35 +0300

    updated func

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
+/**
+* _strlen - returns length of string
+* @str: input character array
+* Return: returns size of string
+* Author: mgemoraw
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (*str != '\0')
+{
+len++;
+}
+return (len);
+}
 /**
 * _strdup - returns pointer array
 * @str: input character array
@@ -11,11 +25,18 @@
 char *_strdup(char *str)
 {
 char *cpy;
-unsigned int len = strlen(str) + 1;
-cpy = malloc(len);
-if (!(cpy))
+unsigned int i, len;
+
+if (str == NULL)
 return (NULL);
-else
-memcpy(cpy, str, len);
+
+len = _strlen(str) + 1;
+cpy = malloc(len);
+if (cpy == NULL)
+return (NULL);
+for (i = 0; i < len; i++)
+cpy[i] = str[i];
+cpy[len] = '\0';
+
 return (cpy);
 }

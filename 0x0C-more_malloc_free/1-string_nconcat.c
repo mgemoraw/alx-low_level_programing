64a0fc1bb16d06f6d9b8482e9a29d3021d2c2f37
mgemoraw <meng.get4@gmail.com>
2023-01-13 00:06:55 +0300

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,22 @@
 #include <stdlib.h>
 
 /**
-*
+* _strlen - returns length of string
+* @str: input character array
+* Return: returns size of string
+* Author: mgemoraw
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (*(str + len) != '\0')
+{
+len++;
+}
+return (len);
+}
+
+/**
 * string_nconcat - equivalent to strncat
 * @s1: destination string
 * @s2: source string
@@ -11,7 +26,34 @@
 * Author: mgemoraw
 */
 
-char *tring_nconcat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+char *str;
+unsigned int size, i, j;
 
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+if (n >= _strlen(s2))
+n = _strlen(s2);
+
+size = _strlen(s1) + n;
+str = malloc(size + 1);
+
+if (str == NULL)
+return (NULL);
+
+for (i = 0; *(s1 + i) != '\0'; i++)
+{
+str[i] = s1[i];
+}
+
+for (j = 0; j < n; j++)
+{
+str[i + j] = s2[j];
+}
+return (str);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,6 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-char *tring_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif

b24054d72fd5c5d70abab966b7903e75103a2681
mgemoraw <meng.get4@gmail.com>
2023-01-13 00:10:47 +0300

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,9 +7,9 @@
 * Return: returns size of string
 * Author: mgemoraw
 */
-int _strlen(char *str)
+unsigned int _strlen(char *str)
 {
-int len = 0;
+unsigned int len = 0;
 while (*(str + len) != '\0')
 {
 len++;

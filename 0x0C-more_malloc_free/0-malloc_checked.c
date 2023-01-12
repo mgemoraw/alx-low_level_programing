443a6fee17fe0f7947cc146547f0d4e1d49c41e1
mgemoraw <meng.get4@gmail.com>
2023-01-12 23:18:43 +0300

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
+#include <unistd.h>
+
+/**
+* _putchar - puts the character string to i/o
+* @c: input character
+* Return: returns charactor to i/o
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
 
 /**
 * _atexit - prints 98
@@ -7,15 +18,16 @@
 */
 void _atexit(void)
 {
-_putchar('' + 98 / 10);
-_putchar('' + 98 % 10);
-_putchar('\n');
+/*
+*_putchar('0' + 98 / 10);
+*_putchar('0' + 98 % 10);
+*_putchar('\n');
+*/
 return;
 }
 
 /**
-*
-* string_nconcat - equivalent to strncat
+* malloc_checked - equivalent to strncat
 * @b: size of string
 * Return: returns nothing
 * Author: mgemoraw
@@ -30,7 +42,7 @@ value = atexit(_atexit);
 
 if (value != 0)
 {
-exit(1);
+exit(98);
 }
 return (str);
 }

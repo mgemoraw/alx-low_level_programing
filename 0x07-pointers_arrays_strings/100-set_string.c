028bb2cea679ea93582526884b0ec9d605f0a3fb
mgemoraw <meng.get4@gmail.com>
2023-01-13 07:59:41 +0300

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*set_string - sets string
+* @s: input stringa rray
+* @to: input string
+* Return: returns nothing
+* Author: mgemoraw
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+return;
+}

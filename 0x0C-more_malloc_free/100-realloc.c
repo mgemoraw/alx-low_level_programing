49927026a7dbd0209b8b0de96c8357ed35011de0
mgemoraw <meng.get4@gmail.com>
2023-01-13 05:31:01 +0300

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _realloc - void func allocates memory
+* @ptr: input pointer
+* @old_size: old size
+* @new_size: new size
+* Return: returns nothin
+* Author: megemoraw
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+void *str;
+char *ptrcpy, *s;
+usigned int i;
+
+if (new_size == old_size)
+return (ptr);
+
+if (ptr == NULL)
+{
+str = malloc(new_size);
+if (str == NULL)
+return (NULL);
+return (str);
+}
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+
+ptrcpy = ptr;
+str = malloc(sizeof(*ptrcpy) * new_size);
+
+if (str == NULL)
+{
+free(ptr);
+return (NULL);
+}
+ptrcpy = str;
+
+for (i = 0; i < old_size && i < new_size; i++)
+{
+s[i] = *ptrcpy++;
+}
+free(ptr);
+return (str);
+}

7d7129ad7e7fda493ea75f43d0c05123771cb584
mgemoraw <meng.get4@gmail.com>
2023-01-17 01:35:58 +0300

    Function-like macro

---
+++ 0x0D-preprocessor/3-functions_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+* File: 3-function_like_macro.h
+* Description: macro for Absolute value of a number
+* Author: mgemoraw
+
+#define ABS(x) (x < 0 ? -x: x)
+
+#endif

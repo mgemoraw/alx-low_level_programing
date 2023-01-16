ff074ee24942323f5329113533dee4c3d111d95d
mgemoraw <meng.get4@gmail.com>
2023-01-16 21:22:28 +0300

    structure function initializer

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include<stdio.h>
+
+/**
+* init_dog - function that initializes struct dog
+* @d: structure dog
+* @name: name of dog
+* @age: age of dog
+* @owner: owner of dog
+*/
+
+void init_dog(struct dog *d, char *name, flaot age, char *owner)
+{
+struct dog d;
+name = d.name;
+age = d.age;
+owner = d.owner;
+return;
+}

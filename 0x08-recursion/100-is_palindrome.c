c15d391c91841a1ffa58c0449adcbb56754a062a
mgemoraw <meng.get4@gmail.com>
2023-01-17 02:37:54 +0300

    Is palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strlen - returns length of string
+* @str: input character array
+* Return: returns size of string
+* Author: mgemoraw
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (*(str + len) != '\0')
+{
+len++;
+}
+return (len);
+}
+
+/**
+* is_palindrome - checks for palindrom text
+* @s: input character array
+* Return: returns 1 if palindrom and 0 if not
+* Author: mgemoraw
+*/
+int is_palindrome(char *s)
+{
+int x = _strlen(s);
+if (x == 0 || x == NULL)
+return (1);
+else
+return (0);
+}

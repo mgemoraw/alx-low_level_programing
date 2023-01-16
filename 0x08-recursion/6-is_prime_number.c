66522b923b49cef83804c499cc17cdb16b145fb3
mgemoraw <meng.get4@gmail.com>
2023-01-17 02:32:36 +0300

    Inception. Is it POssible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,15 +8,15 @@
 int isPrime(int n, int i)
 {
 
-    if (n <= 2)
-        return (n == 2) ? 1 : 0;
-    if (n % i == 0)
-        return 0;
-    if (i * i > n)
-        return 1;
- 
-    /*Check for next divisor*/
-    return isPrime(n, i + 1);
+if (n <= 2)
+return ((n == 2) ? 1 : 0);
+if (n % i == 0)
+return (0);
+if (i * i > n)
+return (1);
+
+/*Check for next divisor*/
+return (isPrime(n, i + 1));
 }
 /**
 * is_prime_number  - checks for prime number

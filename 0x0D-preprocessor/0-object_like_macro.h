28e2d4c1e7e65e7d136f33d24fc1c5fd1e33c732
mgemoraw <meng.get4@gmail.com>
2023-01-17 01:26:13 +0300

    Object-like Macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,12 @@
-#ifndef 0_OBJECT_LIKE_MACRO_H
-#define 0_OBJECT_LIKE_MACRO_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Description: header file defining macro SIZE
+ * Author: mgemoraw
+ */
 #define SIZE 1024
+
 #endif
 

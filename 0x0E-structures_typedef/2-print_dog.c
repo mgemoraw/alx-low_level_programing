8d6d4ba2d0cc6d4b5a005a2ad5b6636b109f4d27
mgemoraw <meng.get4@gmail.com>
2023-01-16 23:02:06 +0300

    final commit of print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,17 +15,17 @@ return;
 }
 
 if (d->name == NULL)
-d->name = "nil";
+d->name = "nil\n";
 else
 printf("Name: %s\n", d->name);
 
-if (d->age < 0)
-printf("nil");
+if (!(d->age))
+printf("nil\n");
 else
 printf("Age: %f\n", d->age);
 
 if (d->owner == NULL)
-d->owner = "nil";
+d->owner = "nil\n";
 else
 printf("Owner: %s\n", d->owner);
 

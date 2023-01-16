5de71922317077aec5003e5b3a018ea9c00bac1f
mgemoraw <meng.get4@gmail.com>
2023-01-16 22:55:28 +0300

    final commit of print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,26 +5,30 @@
 * print_dog - function prints dog
 * @d: structure dog
 * Return: retrns nothing
+* Author: mgemoraw
 */
 void print_dog(struct dog *d)
 {
-
 if (d == NULL)
 {
-
+return;
 }
 
 if (d->name == NULL)
 d->name = "(nil)";
+else
+printf("Name: %s\n", d->name);
 
-if (d->age == 0)
-d->age = 0.0;
+if (d->age < 0)
+{
+printf("(nill)");
+}
+else
+printf("Age: %f\n", d->age);
 
 if (d->owner == NULL)
 d->owner = "(nil)";
-
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
+else
 printf("Owner: %s\n", d->owner);
 return;
 }

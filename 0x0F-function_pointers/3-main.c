a59734073ee75be3867bf2f825cff70484da9ab9
mgemoraw <meng.get4@gmail.com>
2023-01-20 00:18:36 +0300

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+
+/**
+* main - main function
+* @argc: argument count
+* @argv: argument vector
+* Return: returns 0
+*/
+int main(int argc, char *argv[])
+{
+(void);
+int n, m;
+char *op;
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+n = atoi(argv[1]);
+op = argv[2];
+m = atoi(argv[3]);
+
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+
+if ((*op == '/' && m == 0) || (*op == '%' && m == 0))
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(op)(n, m));
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+* op_add - returns sum
+* @a: input number a
+* @b: input integer b
+* Return: returns sum
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub - returns difference
+* @a: input number a
+* @b: input integer b
+* Return: returns difference
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+* op_mul - returns product
+* @a: input number a
+* @b: input integer b
+* Return: returns prodcut
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* op_div- returns division
+* @a: input number a
+* @b: input integer b
+* Return: returns div
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+* op_mod - returns remainder
+* @a: input number a
+* @b: input integer b
+* Return: returns moduli
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}

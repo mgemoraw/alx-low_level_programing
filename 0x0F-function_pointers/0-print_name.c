e532b0ee90c827e21a83fa37ce3c7426fca41f90
mgemoraw <meng.get4@gmail.com>
2023-01-19 07:00:24 +0300

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+* print_name - prints name
+* @name: name pointer
+* @f: function pointer
+* Return: returns nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+    f(name);
+}

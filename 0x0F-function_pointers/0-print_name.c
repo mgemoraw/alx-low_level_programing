2e058c17ba290d9a9dfb7d14f1817217db616a7b
mgemoraw <meng.get4@gmail.com>
2023-01-19 23:38:39 +0300

    final commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
 * print_name - prints name
@@ -9,7 +8,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL)
+if (name == NULL || f == NULL)
 return;
 
 (*f)(name);

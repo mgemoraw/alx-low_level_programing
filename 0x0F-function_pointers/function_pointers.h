b683d8fbd690b2dcf03d7a22eef5f3a1db07a540
mgemoraw <meng.get4@gmail.com>
2023-01-19 12:51:49 +0300

    mini fix

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#define size_t int
+#define size_t unsigned int
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif

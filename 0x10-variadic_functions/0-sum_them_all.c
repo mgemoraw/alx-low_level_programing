2196c2908a9700436afeb1980582c3b8f819c21a
mgemoraw <meng.get4@gmail.com>
2023-01-20 05:59:13 +0300

    first commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+*sum_them_all - sums all its parameters
+*@n: first parameter
+*Return: 0 if n is null
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+va_list list;
+int sum = 0;
+unsigned int i;
+
+va_start(list, n);
+
+if (n != 0)
+{
+for (i = 0; i < n; i++)
+sum += va_arg(list, int);
+}
+
+va_end(list);
+return (sum);
+
+}

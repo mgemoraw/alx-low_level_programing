837a86e4dd65730ea3466e8a282ccff4e67fb8eb
mgemoraw <meng.get4@gmail.com>
2023-01-20 07:38:09 +0300

    To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list list;
 unsigned int i;
 
-if (n == 0)
-return;
+// if (n == 0)
+// return;
 
 va_start(list, n); /*sart of argument array */
 for (i = 0; i < n; i++)

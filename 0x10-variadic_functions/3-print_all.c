21163a9e8222a5d1e36c995302589e17ba9d366a
mgemoraw <meng.get4@gmail.com>
2023-01-21 18:58:30 +0300

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,53 +7,44 @@
 */
 void print_all(const char * const format, ...)
 {
-    char flag;
-    /*char *fmt;*/
-    char *str;
-    unsigned int i = 0, len = 0;
-    va_list list;
-/*
-    if (format == NULL)
-    return;
-*/
-    while (format != NULL && *(format + len)!= '\0')
-    len++;
+	char flag;
+	/*char *fmt;*/
+	char *str;
+	unsigned int i = 0, len = 0;
+	va_list list;
 
+	while (format != NULL && *(format + len) != '\0')
+		len++;
 
-    va_start(list, format);
-
-    while (i < len )
-    {
-        flag = format[i];
-       
-        switch(flag)
-        {
-        case 'c':
-            printf("%c", va_arg(list, int));
-            break;
-        case 'i':
-            printf("%i", va_arg(list, int));
-            break;
-        case 's':
-            str = va_arg(list, char*);
-            if (str == NULL)
-                str = "(nil)";
-            printf("%s", str);
-            break;
-        case 'f':
-            printf("%f", va_arg(list, double));
-            break;
-        default: 
-            i++;
-            continue;
-        }
-        if (i < len-1) 
-        printf(", ");
-        i++;
-        
-    }
-    va_end(list);
-
-    printf("\n");
+	va_start(list, format);
+	while (i < len)
+	{
+		flag = format[i];
+		switch (flag)
+		{
+			case 'c':
+				printf("%c", va_arg(list, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(list, int));
+				break;
+			case 's':
+				str = va_arg(list, char*);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;
+			default:
+				i++;
+				continue;
+		}
+		if (i < len - 1)
+			printf(", ");
+		i++;
+	}
+	va_end(list);
+	printf("\n");
 }
-

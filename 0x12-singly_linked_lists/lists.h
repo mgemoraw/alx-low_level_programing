f071d0ea87d4b849ea7d6c84d22aacb951ffb3c1
mgemoraw <meng.get4@gmail.com>
2023-01-27 00:23:20 +0300

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -29,6 +29,7 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void pre_main(void);
 
 
 #endif

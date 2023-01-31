86681a7bfefa81edb206102082a39190dd3e4dab
mgemoraw <meng.get4@gmail.com>
2023-02-01 00:10:30 +0300

    free listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+*free_listint - removes allocated memory to listint_t
+*@head: input structure node
+*Return: returns nothing
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *currentNode;
+
+	while (head != NULL)
+	{
+		currentNode = head;
+		head = head->next;
+		free(currentNode->n);
+		free(currentNode);
+	}
+}

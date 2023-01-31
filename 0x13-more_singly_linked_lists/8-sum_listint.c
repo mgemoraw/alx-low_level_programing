bf8e3123995cc120134732e52707af93e19c3d9b
mgemoraw <meng.get4@gmail.com>
2023-02-01 01:58:21 +0300

    sum of linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* sum_listint - sums data in linked list
+*@head: head node
+*Return: eturns sum of the linked list
+*/
+int sum_listint(listint_t *head)
+{
+    int sum = 0, i = 0;
+    while (head != NULL)
+    {
+        if (head->n != 0)
+        {
+            sum += head->n;
+            head = head->next;
+        }
+        i++;
+    }
+    return (sum);
+}

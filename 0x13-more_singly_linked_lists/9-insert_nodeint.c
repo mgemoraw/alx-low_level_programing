7a4c0ee065e0cd11774676d4265c770b6a1c9d86
mgemoraw <meng.get4@gmail.com>
2023-02-01 02:28:32 +0300

    add node at index i

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,9 +18,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (head != NULL)
 	{
 		head = head->next;
-		if (i == idx)
+		if (head->n != 0 && i == idx)
 		{
 			newNode->next = head->next;
+			newNode->n = n;
 			head->next = newNode;
 		}
 		i++;

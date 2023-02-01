9df4da92afb932f06102cf6e4b144e56ac1daa0f
mgemoraw <meng.get4@gmail.com>
2023-02-02 01:42:55 +0300

    two final advanced projects

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees memory of a linked list
+ * @h: linked head pointer
+ * Return: node
+ */
+size_t free_listint_safe(listint_t **h)
+{
+
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds a node in a linked list
+ * @head: linked list
+ * Return: node
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+
+}

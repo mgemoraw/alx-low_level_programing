998873b2225fd67c90d2dc44e338daf5e934f0f7
mgemoraw <meng.get4@gmail.com>
2023-02-01 03:50:06 +0300

    pop list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,10 +9,10 @@ int pop_listint(listint_t **head)
 	listint_t *currentNode
 	int value;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
-	value = current->n;
+	value = (*head)->n;
 	currentNode = (*head)->next;
 	free(*head);
 	*head = currentNode;

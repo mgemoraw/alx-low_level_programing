34a17c1b86ccba47314b59afc9ecdb0abe09adf2
mgemoraw <meng.get4@gmail.com>
2023-02-02 21:35:33 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,13 +6,14 @@
 */
 unsigned int _strlen(const char *b)
 {
-    unsigned int len = 0, i = 0;
-    while (*(b + i) != 0)
-    {
-        len += 1;
-        i++;
-    }
-    return (len);
+	unsigned int len = 0, i = 0;
+
+	while (*(b + i) != 0)
+	{
+		len += 1;
+		i++;
+	}
+	return (len);
 }
 /**
 * binary_to_uint - converts binary to unsigned int
@@ -21,18 +22,19 @@ unsigned int _strlen(const char *b)
 */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0, pro = 0, i, j;
-    for (i = 0; *(b + i) != '\0'; i++)
-    {
-        if (*(b + i) == '0')
-            continue;
-        else
-        {
-            pro = 1;
-            for (j = 0; j < _strlen(b+i) - 1; j++)
-                pro *= 2;
-        }
-        num += pro;
-    }
-    return (num);
+	unsigned int num = 0, pro = 0, i, j;
+
+	for (i = 0; *(b + i) != '\0'; i++)
+	{
+		if (*(b + i) == '0')
+			continue;
+		else
+		{
+			pro = 1;
+			for (j = 0; j < _strlen(b + i) - 1; j++)
+				pro *= 2;
+		}
+		num += pro;
+	}
+	return (num);
 }

08fa71be1bcf0c2002f0ebf5657cb62b52f79f7c
mgemoraw <meng.get4@gmail.com>
2023-02-07 20:32:36 +0300

    setting up files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*read_textfile - read text files
+*@filename: file name to be read
+*@letters: size of chars in filename
+*Return: retruns ssize_t
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}

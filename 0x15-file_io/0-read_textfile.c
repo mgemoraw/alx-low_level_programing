2cde2a59956fad83050658c30c1909733ea8d06c
mgemoraw <sgetme@outlook.com>
2023-02-10 11:15:45 -0800

    nothing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,7 +7,7 @@
 */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 /**
 *read_textfile - read text files
@@ -17,30 +17,31 @@ int _putchar(char c)
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file, chars_in, chars_out;
-    char *buffer;
+	ssize_t file, chars_in, chars_out;
+	char *buffer;
 
-    buffer = malloc(sizeof(char) * letters);
+	buffer = malloc(sizeof(char) * letters);
 
-    if (buffer == NULL || filename == NULL)
-        return (0);
+	if (buffer == NULL || filename == NULL)
+		return (0);
 
-    file = open(filename, O_RDONLY);
+	file = open(filename, O_RDONLY);
 
-    if (file == -1)
-        return (0);
+	if (file == -1)
+		return (0);
 
-    chars_in = read(file, buffer, letters);
-    if (chars_in == -1)
-        return (0);
+	chars_in = read(file, buffer, letters);
+	if (chars_in == -1)
+		return (0);
 
-    chars_out = write(STDOUT_FILENO, buffer, chars_in);
+	chars_out = write(STDOUT_FILENO, buffer, chars_in);
 
-    if (chars_out == -1)
-        return (0);
+	if (chars_out == -1)
+		return (0);
     
-    close(file);
-    free(buffer);
-    return (chars_out);
+	close(file);
+	free(buffer);
+	return (chars_out);
     
 }
+

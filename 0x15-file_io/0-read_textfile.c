44fd7f4646ea68d004a24d03a9a11358003da1d9
mgemoraw <meng.get4@gmail.com>
2023-02-10 19:53:56 +0300

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
 /**
+* _putchar - puts character to the screen
+* @c: input character
+* Return: returns chacter
+*/
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+/**
 *read_textfile - read text files
 *@filename: file name to be read
 *@letters: size of chars in filename
@@ -9,14 +18,26 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     FILE *f_out;
-    char *str;
+    char ch;
+    ssize_t chars = 0;
 
-    fout = fopen(filename, "r");
+    f_out = fopen(filename, "r");
 
     if (f_out == NULL || filename == NULL)
         return (0);
 
-    
+    while((ch = fgetc(f_out)) != EOF)
+    {
+        if (ch =='\n')
+        {
+            _putchar('\n');
+        }
+        else
+        {
+            _putchar(ch);
+        }
+        chars++; 
+    }
 
-
+    return (chars + 1);
 }

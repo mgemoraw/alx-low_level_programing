e444101b84867fc2ce9d502383396387a5402fc0
mgemoraw <meng.get4@gmail.com>
2023-02-21 09:49:04 +0300

    final commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,26 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int file, len = 0, out;
+int file, i, len = 0, out;
 
 
 if (filename == NULL)
 return (0);
 
-if (text_content != NULL)
-{
-for (len = 0; text_content[len]; len++)
-len++;
-}
 file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+if (file == -1)
+return (-1);
+
+if (!text_content)
+text_content = "";
+
+
+for (i = 0; text_content[i]; i++)
+len++;
 out = write(file, text_content, len);
 
-if (file == -1 || out == -1)
+if (out == -1)
 return (-1);
 close(file);
 return (1);

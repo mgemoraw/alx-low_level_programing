76f9a1d6c5809f4c5f62459b697a77afa3fd87dc
mgemoraw <meng.get4@gmail.com>
2023-02-21 10:15:37 +0300

    Finally worked

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,24 +7,25 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int file, fout, i;
+	int file, len = 0, fout, i;
+	if (filename == NULL)
+		return (-1);
 
-if (filename == NULL)
-return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
 
-file = open(filename, O_APPEND | O_RDONLY, 0664);
+	if (file == -1)
+		return (-1);
 
-if (file == -1)
-return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+			len++;
 
-if (text_content != NULL)
-{
-for (i = 0; text_content[i]; i++)
-fout = write(file, text_content, i);
+		fout = write(file, text_content, len);
 
-if (fout == -1)
-return (-1);
-}
-close(file);
-return (1);
+		if (fout == -1)
+			return (-1);
+	}
+	close(file);
+	return (1);
 }

3e1644cb5cdcf1445be71d69aa7153102bf4da34
mgemoraw <meng.get4@gmail.com>
2023-03-16 19:44:15 +0300

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints members of double linked list
+ * @h: head of linked list
+ * Return: returns size of linked list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+	const dlistint_t *temp = malloc(sizeof(dlistint_t));
+
+
+	if (h == NULL)
+		return (size);
+
+
+	temp = h;
+
+	while(temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		size++;
+	}
+	return (size);
+}

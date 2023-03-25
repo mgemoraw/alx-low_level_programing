a7476c8bbc6826530714f4deab4f31538d22d764
mgemoraw <meng.get4@gmail.com>
2023-03-25 21:47:53 +0300

    free list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -11,19 +11,21 @@ void free_dlistint(dlistint_t *head)
 	if (head == NULL)
 		return;
 
-	if (temp->next == NULL)
-	{
+	if (head->next == NULL)
 		free(head);
-	}
-	else
+
+	temp = head;
+	if (temp->prev != NULL)
 	{
+		temp = temp->prev;
+	}
+
 	while (temp->next != NULL)
 	{
 		head = temp->next;
 		head->prev = NULL;
 
 		free(temp); /* free current head */
-	}
 	}
 
 }

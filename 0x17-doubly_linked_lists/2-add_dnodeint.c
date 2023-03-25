e2f728db9bf3139d9475109f3c750f9b6426e984
mgemoraw <meng.get4@gmail.com>
2023-03-25 15:18:36 +0300

    Add node at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,22 +13,46 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	/*temp = malloc(sizeof(dlistint_t)); */
 	newNode = malloc(sizeof(dlistint_t));
 
-
-	newNode->n = n;
 	newNode->prev = NULL;
+	newNode->n = n;
+	newNode->next = NULL;
 
 	if (*head == NULL)
 	{
 		newNode->next = NULL;
-		*head = newNode;
+		(*head) = newNode;
 	}
 	else
 	{
-		newNode->next = (*head);
 		(*head)->prev = newNode;
+		newNode->next = (*head);
 		(*head) = newNode;
 	}
 	
 	/* return (newNode); */
 	return (*head);
+}
+
+/**
+ * _print_dlistint_backward - prints linked list in the backward direction
+ * @head: head of the double linked list 
+ * @Return: retusns size of the list
+ */
+size_t _print_dlistint_backward(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	size_t counter = 0;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+	}
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->prev;
+		counter++;
+	}
+
+	return (counter);
 }

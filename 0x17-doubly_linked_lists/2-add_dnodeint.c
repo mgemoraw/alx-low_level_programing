2e7d34f1a244e54a43da69ec9e5d3fcd79fe2862
mgemoraw <meng.get4@gmail.com>
2023-03-25 20:43:13 +0300

    say Hello to adding ode

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+* add_dnodeint - adds node at the begining
+* @head: pointer to head of the list
+* @n: data to be added
+* Return: returns the head of the list
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	/*dlistint_t *temp, *newNode; */
+	dlistint_t *newNode;
+	/*temp = malloc(sizeof(dlistint_t)); */
+	newNode = malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->prev = NULL;
+	newNode->n = n;
+	newNode->next = NULL;
+
+
+	if (*head == NULL)
+	{
+	newNode->prev = NULL;
+	newNode->n = n;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		newNode->next = NULL;
+		(*head) = newNode;
+	}
+	else
+	{
+		newNode->next = *head;
+		(*head)->prev = newNode;
+	}
+
+	/* return (newNode); */
+	return (newNode);
+}

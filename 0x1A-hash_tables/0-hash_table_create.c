7a30a93d156ec044b357b82aa4300530c613c542
mgemoraw <meng.get4@gmail.com>
2023-07-03 13:56:14 +0300

    create hashtables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#!/bin/bash
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash table
+ * @size: size of hash table
+ * Return: returns hash table structure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i = 0;
+	hash_table_t *ht = malloc(sizeof(hash_table_t);
+
+	if (ht == NULL)
+	{
+		fprintf(stderr, "Error: mallock failed\n");
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
+

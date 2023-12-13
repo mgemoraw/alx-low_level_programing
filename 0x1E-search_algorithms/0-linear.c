ed8335df695bda27355e6f8057773176d6475080
mgemoraw <meng.get4@gmail.com>
2023-12-13 14:09:46 +0300

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,12 +2,12 @@
 
 
 /**
- * linear_search - linear search implementation of c 
- * 
- * @param array: pointer to first element of the array
- * @param size: size of array
- * @param value: target value of the array
- * @return int 
+ * linear_search - linear search implementation of c
+ *
+ * @array: pointer to first element of the array
+ * @size: size of array
+ * @value: target value of the array
+ * @return: int
  */
 int linear_search(int *array, size_t size, int value)
 {

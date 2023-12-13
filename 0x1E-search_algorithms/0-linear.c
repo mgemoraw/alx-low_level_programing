c7b9b1488664c129c4c7b2d5ca82b3625eead646
mgemoraw <meng.get4@gmail.com>
2023-12-13 14:11:43 +0300

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,8 +11,10 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	for (int i = 0; i < size; i++) {
-		if (array[i] == value) {
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+		{
 			return (i);
 		}
 	}
